f0f787424ef9f8139cf8873f566c005fdaa3f39d
gluk71 <111636696+gluk71@users.noreply.github.com>
2023-01-23 10:10:58 +0300

    Create Class_Animal.java

---
+++ Seminar_2/Class_Animal.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+class ListAnimals{
+    public String bAnimal;
+    public String bName;
+
+}
+public class Klass_animals extends Animals{
+    public String AnName;
+    public Klass_animals(String anName){
+        this.AnName = anName;
+    }
+
+    ArrayList<ListAnimals> listAnimals = new ArrayList<>();
+
+    public void Add_Animal(String AName, String animalsName){
+        ListAnimals lb = new ListAnimals();
+        SetAnimals(AName);
+        SetAnimalsName(animalsName);
+
+        lb.bAnimal = GetAnimals();
+        lb.bName = GetAnimalsName();
+
+        listAnimals.add(lb);
+    }
+
+    public ArrayList<ListAnimals> getBookList(){
+        return listAnimals;
+    }
+}
