0020ca472fbe02366c21506454afee581834893b
gluk71 <111636696+gluk71@users.noreply.github.com>
2023-01-23 10:01:26 +0300

    Update Main.java

--- Seminar_2/Main.java
+++ Seminar_2/Main.java
@@ -2,17 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        Persona father = new Persona("Roman", "Men", 63, "Father");
-        Persona son = new Persona("Slavka", "Men", 43, "Son");
-        Persona daughter = new Persona("Katerina", "Women", 25, "Daughter");
-        Persona granddaughter = new Persona("Inna", "Women", 7, "Granddaughter");
+        Person daughter = new Persona("Elvira", "Women", 25, "Daughter");
+        Person granddaughter = new Persona("Tamara", "Women", 7, "Granddaughter");
+        Person father = new Persona("Ioann", "Men", 63, "Father");
+        Person son = new Persona("Slavka", "Men", 43, "Son");
 
         father.PersAnimal.AddAnimal("Fish", "Rodka");
         father.PersAnimal.AddAnimal("Fish", "Borka");
 
-        son.PersAnimal.AddAnimal("Cat", "Gnom");
-        son.PersAnimal.AddAnimal("Dog", "Ara");
+        son.PersAnimal.AddAnimal("Cat", "Pluh");
+        son.PersAnimal.AddAnimal("Dog", "Rex");
 
         daughter.PersAnimal.AddAnimal("Snake", "Gooroo");
 
