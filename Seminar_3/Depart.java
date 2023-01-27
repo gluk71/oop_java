bc5b6022b8b7494d37171cedf4a0916e1f2ddab9
gluk71 <111636696+gluk71@users.noreply.github.com>
2023-01-27 06:52:46 +0300

    Create Depart.java

---
+++ Seminar_3/Depart.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Department {
+    String deportName;
+    String deportWorker;
+
+    ArrayList<Persona> personaInDept = new ArrayList<>();
+    // dep0, dep1, dep2
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+}
