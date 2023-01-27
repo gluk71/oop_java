49e601607e1ef33697a185d1e5fb220da8c08243
gluk71 <111636696+gluk71@users.noreply.github.com>
2023-01-27 06:55:28 +0300

    Create Group.java

---
+++ Seminar_3/Group.java
@@ -0,0 +1,15 @@
+import javax.swing.plaf.PanelUI;
+import java.util.ArrayList;
+
+public class Group extends Department {
+
+
+    public Group(String GropName){
+        setDeptName(GropName);
+    }
+
+    public void addPeople(Persona persona){
+        personaInDept.add(persona);
+    }
+
+}
