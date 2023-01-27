c503cf639bdbe73c699ee4a2956bac2507320f6a
gluk71 <111636696+gluk71@users.noreply.github.com>
2023-01-27 07:21:14 +0300

    Create Incorporated.java

---
+++ Seminar_3/Incorporated.java
@@ -0,0 +1,17 @@
+public class incorporated {
+    String corpName;
+    String corpDept;
+
+    public void incorporated(String CorpName, String CorpDept){
+        this.corpName = CorpName;
+        this.corpDept = CorpDept;
+    }
+
+    public String getCorpName(){
+        return corpName;
+    }
+
+    public String getCorpDept(){
+        return corpName;
+    }
+}
