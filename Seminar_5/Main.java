d194d1f0afe753f4b4e83564ddbb08ea5c7107e6
gluk71 <111636696+gluk71@users.noreply.github.com>
2023-02-02 09:27:33 +0300

    Update Main.java

--- Seminar_5/Main.java
+++ Seminar_5/Main.java
@@ -1,1 +1,49 @@
-1
+package org.phonebook;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+class LogApp {
+    /**
+     * Старт программы
+     * @param args не используется
+     */
+    public static void main(String[] args)
+    {
+        // Получаем экземпляр класса, который будем использовать для записи логов.
+        // Передаваемый параметр LogApp - имя логера.
+        // В документации сказано, что имя логгера должно совпадать с именем
+        // класса или пакета ().
+        Logger logger = Logger.getLogger(LogApp.class.getName());
+        Menu.mainMenu();
+
+        try {
+            // Оставляем предыдущий handler (будет создаваться файл "LogApp")
+            FileHandler fh = new FileHandler("%tLogApp");
+            logger.addHandler(fh);
+
+
+            // Добавляем ещё файл "LogApp.htm".
+            HtmlFormatter htmlformatter = new HtmlFormatter();
+            FileHandler htmlfile = new FileHandler("%tLogApp.htm");
+            // Устанавливаем html форматирование с помощью класса HtmlFormatter.
+            htmlfile.setFormatter(htmlformatter);
+            logger.addHandler(htmlfile);
+
+
+        } catch (SecurityException e) {
+            logger.log(Level.SEVERE,
+                    "Не удалось создать файл лога из-за политики безопасности.",
+                    e);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE,
+                    "Не удалось создать файл лога из-за ошибки ввода-вывода.",
+                    e);
+        }
+
+        logger.log(Level.INFO, "Запись лога с уровнем INFO (информационная)");
+        logger.log(Level.WARNING,"Запись лога с уровнем WARNING (Предупреждение)");
+        logger.log(Level.SEVERE, "Запись лога с уровнем SEVERE (серъёзная ошибка)",
+                new Exception("Проверочное сообщение об ошибке"));
+    }
+}
